f71c5cebd2ae899758e8472aa68dfabb258c3c9b
Edward Hanson <ehanson787@gmail.com>
2025-06-08 23:34:57 +0000

    A simple spring app to test ecr

---
+++ src/main/java/com/lab/ecr/controller/PageController.java
@@ -0,0 +1,22 @@
+package com.lab.ecr.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class PageController {
+
+    @GetMapping("/")
+    public String greeting(@RequestParam(name = "name", required = false, defaultValue = "Guest") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+
+    @GetMapping("/about")
+    public String about(Model model) {
+        model.addAttribute("info", "This app is a lab to demonstrate how to dockerize and push spring apps to ECS.");
+        return "about";
+    }
+}
